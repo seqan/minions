9a80756c26bb13a6d9fe67dead16d1f63dc57951
Mitra Darja Darvish <mdarvish@posteo.de>
2021-08-04 12:57:32 +0200

    [MISC] Change main.cpp to project.

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,9 @@
 #include <seqan3/argument_parser/all.hpp>
 #include <seqan3/core/debug_stream.hpp>
 
-#include "fastq_conversion.hpp"
-
 int main(int argc, char ** argv)
 {
-    seqan3::argument_parser parser{"Fastq-to-Fasta-Converter", argc, argv};
+    seqan3::argument_parser parser{"comparison", argc, argv};
 
     // Declarations for argument parser
     std::filesystem::path fastq_file{};
@@ -15,13 +13,8 @@ int main(int argc, char ** argv)
     bool verbose = false;
 
     // Parser
-    parser.info.author = "SeqAn-Team"; // give parser some infos
-    parser.info.version = "1.0.0";
-    parser.add_positional_option(fastq_file, "Please provide a fastq file.",
-                                 seqan3::input_file_validator{{"fq","fastq"}}); // Takes a fastq file and validates it
-    //output path as option, otherwise output is printed
-    parser.add_option(output_file, 'o', "output", "The file for fasta output. Default: stdout");
-    parser.add_flag(verbose, 'v', "verbose", "Give more detailed information here."); // example for a flag
+    parser.info.author = "Mitra Darvish"; // give parser some infos
+    parser.info.version = "0.1.0";
 
     try
     {
@@ -32,13 +25,6 @@ int main(int argc, char ** argv)
         seqan3::debug_stream << "Parsing error. " << ext.what() << "\n"; // give error message
         return -1;
     }
-
-    convert_fastq(fastq_file, output_file); // Call fastq to fasta converter
-
-    if (verbose) // if flag is set
-        seqan3::debug_stream << "Conversion was a success. Congrats!\n";
-
-
 
     return 0;
 }
