15d7d2afc53ef0b685b5da57b8fc658e4affe313
Mitra Darja Darvish <mdarvish@posteo.de>
2022-11-24 11:56:56 +0100

    [FEATURE] Add randstrobe hash.

---
+++ include/randstrobe_hash.hpp
@@ -0,0 +1,298 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \author Hossein Eizadi Moghadam <hosseinem AT fu-berlin.de>
+ * \brief Provides randstrobe_hash.
+ */
+
+#pragma once
+
+#include <seqan3/alphabet/views/complement.hpp>
+#include <seqan3/core/detail/strong_type.hpp>
+#include <seqan3/search/views/kmer_hash.hpp>
+#include <seqan3/search/views/minimiser_hash.hpp>
+#include <seqan3/utility/views/zip.hpp>
+
+#include "randstrobe.hpp"
+#include "shared.hpp"
+
+uint64_t combine_strobes(uint64_t multiplicator, uint64_t first_strobe, uint64_t second_strobe)
+{
+    return first_strobe*multiplicator + second_strobe;
+}
+
+uint64_t combine_strobes(uint64_t multiplicator, uint64_t multiplicator2, uint64_t first_strobe, uint64_t second_strobe, uint64_t third_strobe)
+{
+    return first_strobe*multiplicator + second_strobe*multiplicator2 + third_strobe;
+}
+
+namespace seqan3::detail
+{
+//!\brief seqan3::views::randstrobe2_hash's range adaptor object type (non-closure).
+//!\ingroup search_views
+struct randstrobe2_hash_fn
+{
+    /*!\brief Store the shape and the window min and max offsets and return a range adaptor closure object.
+    * \param[in] shape       The seqan3::shape to use for hashing.
+    * \param[in] window_min  The lower offset for the position of the next window from the previous one.
+    * \param[in] window_len  The upper offset for the position of the next window from the previous one.
+    * \throws std::invalid_argument if window_min is greater than window_len or smaller than 1.
+    * \returns               A range of converted elements in vectors of size 2.
+    */
+    constexpr auto operator()(shape const & shape, uint32_t const window_min, uint32_t const window_len) const
+    {
+        return seqan3::detail::adaptor_from_functor{*this, shape, window_min, window_len};
+    }
+
+    /*!\brief Store the shape, the window size and the seed and return a range adaptor closure object.
+    * \param[in] shape       The seqan3::shape to use for hashing.
+    * \param[in] window_min  The lower offset for the position of the next window from the previous one.
+    * \param[in] window_len  The upper offset for the position of the next window from the previous one.
+    * \param[in] seed        The seed to use.
+    * \throws std::invalid_argument if window_min is greater than window_len or smaller than 1.
+    * \returns               A range of converted elements in vectors of size 2.
+    */
+    constexpr auto operator()(shape const & shape, uint32_t const window_min, uint32_t const window_len, seed const seed) const
+    {
+        return seqan3::detail::adaptor_from_functor{*this, shape, window_min, window_len, seed};
+    }
+
+    /*!\brief Call the view's constructor with the underlying view, a seqan3::shape and a window size as argument.
+     * \param[in] urange      The input range to process. Must model std::ranges::viewable_range and the reference type
+     *                        of the range must model seqan3::semialphabet.
+     * \param[in] shape       The seqan3::shape to use for hashing.
+     * \param[in] window_min  The lower offset for the position of the next window from the previous one.
+     * \param[in] window_len  The length of a window.
+     * \param[in] seed        The seed to use.
+     * \throws std::invalid_argument if window_min is greater than window_len or smaller than 1.
+     * \returns               A range of converted elements in vectors of size 2.
+     */
+    template <std::ranges::range urng_t>
+    constexpr auto operator()(urng_t && urange,
+                              shape const & shape,
+                              uint32_t const window_min,
+                              uint32_t const window_len,
+                              seed const seed = seqan3::seed{0x8F3F73B5CF1C9ADE}) const
+    {
+        static_assert(std::ranges::viewable_range<urng_t>,
+            "The range parameter to views::randstrobe_hash cannot be a temporary of a non-view range.");
+        static_assert(std::ranges::forward_range<urng_t>,
+            "The range parameter to views::randstrobe_hash must model std::ranges::forward_range.");
+        static_assert(semialphabet<std::ranges::range_reference_t<urng_t>>,
+            "The range parameter to views::randstrobe_hash must be over elements of seqan3::semialphabet.");
+
+        if (window_len < window_min)
+            throw std::invalid_argument{"The chosen parameters are not valid. "
+                                        "Please choose a window_len greater than window_min."};
+
+        auto hashed_values = std::forward<urng_t>(urange) | seqan3::views::kmer_hash(shape)
+                                                          | std::views::transform([seed] (uint64_t i)
+                                                                                  {return i ^ seed.get();});
+
+
+        auto randstrobes = seqan3::detail::randstrobe_view(hashed_values, window_min + shape.size() - 1, window_len - shape.size() + 1);
+        uint64_t multiplicator = std::pow(4, shape.count());
+        auto forward = std::views::transform(randstrobes, [multiplicator] (std::vector<uint64_t> i)
+                           {return combine_strobes(multiplicator, i[0], i[1]);});
+
+        auto rev_hashed_values = std::forward<urng_t>(urange) | seqan3::views::complement
+                                                              | std::views::reverse
+                                                              | seqan3::views::kmer_hash(shape)
+                                                              | std::views::transform([seed] (uint64_t i)
+                                                                                     {return i ^ seed.get();});
+
+
+        auto rev_randstrobes = std::views::reverse(seqan3::detail::randstrobe_view(rev_hashed_values, window_min + shape.size() - 1, window_len - shape.size() + 1));
+        auto reverse = std::views::transform(rev_randstrobes, [multiplicator] (std::vector<uint64_t> i)
+                          {return combine_strobes(multiplicator, i[0], i[1]);});
+
+        return seqan3::views::zip(forward, reverse) | std::views::transform([](std::tuple<uint64_t, uint64_t> i){return std::min(std::get<0>(i), std::get<1>(i));});
+
+    }
+};
+
+//!\brief seqan3::views::randstrobe3_hash's range adaptor object type (non-closure).
+//!\ingroup search_views
+struct randstrobe3_hash_fn
+{
+    /*!\brief Store the shape and the window min and max offsets and return a range adaptor closure object.
+    * \param[in] shape       The seqan3::shape to use for hashing.
+    * \param[in] window_min  The lower offset for the position of the next window from the previous one.
+    * \param[in] window_len  The upper offset for the position of the next window from the previous one.
+    * \throws std::invalid_argument if window_min is greater than window_len or smaller than 1.
+    * \returns               A range of converted elements in vectors of size 2.
+    */
+    constexpr auto operator()(shape const & shape, uint32_t const window_min, uint32_t const window_len) const
+    {
+        return seqan3::detail::adaptor_from_functor{*this, shape, window_min, window_len};
+    }
+
+    /*!\brief Store the shape, the window size and the seed and return a range adaptor closure object.
+    * \param[in] shape       The seqan3::shape to use for hashing.
+    * \param[in] window_min  The lower offset for the position of the next window from the previous one.
+    * \param[in] window_len  The upper offset for the position of the next window from the previous one.
+    * \param[in] seed        The seed to use.
+    * \throws std::invalid_argument if window_min is greater than window_len or smaller than 1.
+    * \returns               A range of converted elements in vectors of size 2.
+    */
+    constexpr auto operator()(shape const & shape, uint32_t const window_min, uint32_t const window_len, seed const seed) const
+    {
+        return seqan3::detail::adaptor_from_functor{*this, shape, window_min, window_len, seed};
+    }
+
+    /*!\brief Call the view's constructor with the underlying view, a seqan3::shape and a window size as argument.
+     * \param[in] urange      The input range to process. Must model std::ranges::viewable_range and the reference type
+     *                        of the range must model seqan3::semialphabet.
+     * \param[in] shape       The seqan3::shape to use for hashing.
+     * \param[in] window_min  The lower offset for the position of the next window from the previous one.
+     * \param[in] window_len  The length of a window.
+     * \param[in] seed        The seed to use.
+     * \throws std::invalid_argument if window_min is greater than window_len or smaller than 1.
+     * \returns               A range of converted elements in vectors of size 2.
+     */
+    template <std::ranges::range urng_t>
+    constexpr auto operator()(urng_t && urange,
+                              shape const & shape,
+                              uint32_t const window_min,
+                              uint32_t const window_len,
+                              seed const seed = seqan3::seed{0x8F3F73B5CF1C9ADE}) const
+    {
+        static_assert(std::ranges::viewable_range<urng_t>,
+            "The range parameter to views::randstrobe_hash cannot be a temporary of a non-view range.");
+        static_assert(std::ranges::forward_range<urng_t>,
+            "The range parameter to views::randstrobe_hash must model std::ranges::forward_range.");
+        static_assert(semialphabet<std::ranges::range_reference_t<urng_t>>,
+            "The range parameter to views::randstrobe_hash must be over elements of seqan3::semialphabet.");
+
+        if (window_min < 1 || window_len < window_min)
+            throw std::invalid_argument{"The chosen parameters are not valid. "
+                                        "Please choose values greater than 0 and a window_len greater than window_min."};
+
+        auto hashed_values = std::forward<urng_t>(urange) | seqan3::views::kmer_hash(shape)
+                                                          | std::views::transform([seed] (uint64_t i)
+                                                                                  {return i ^ seed.get();});
+
+
+        auto randstrobes = seqan3::detail::randstrobe_view<decltype(hashed_values), 3>(hashed_values, window_min + shape.size() - 1, window_len - shape.size() + 1);
+        uint64_t multiplicator = std::pow(4, shape.count()*2);
+        uint64_t multiplicator2 = std::pow(4, shape.count());
+        auto forward = std::views::transform(randstrobes, [multiplicator, multiplicator2] (std::vector<uint64_t> i)
+                           {return combine_strobes(multiplicator, multiplicator2, i[0], i[1], i[2]);});
+
+
+        auto rev_hashed_values = std::forward<urng_t>(urange)  | seqan3::views::complement
+                                                             | std::views::reverse
+                                                             | seqan3::views::kmer_hash(shape)
+                                                             | std::views::transform([seed] (uint64_t i)
+                                                                                    {return i ^ seed.get();});
+
+        auto rev_randstrobes =  std::views::reverse(seqan3::detail::randstrobe_view<decltype(rev_hashed_values), 3>(rev_hashed_values, window_min + shape.size() - 1, window_len - shape.size() + 1));
+        auto reverse = std::views::transform(rev_randstrobes, [multiplicator, multiplicator2] (std::vector<uint64_t> i)
+                         {return combine_strobes(multiplicator, multiplicator2, i[0], i[1], i[2]);});
+
+        return seqan3::views::zip(forward, reverse) | std::views::transform([](std::tuple<uint64_t, uint64_t> i){return std::min(std::get<0>(i), std::get<1>(i));});
+    }
+};
+
+} // namespace seqan3::detail
+
+/*!\name Alphabet related views
+ * \{
+ */
+
+/*!\brief                    Computes randstrobes for a range with a given shape, min and max window offsets and seed.
+ * \tparam urng_t            The type of the range being processed. See below for requirements. [template parameter is
+ *                           omitted in pipe notation]
+ * \param[in] urange         The range being processed. [parameter is omitted in pipe notation]
+ * \param[in] shape          The seqan3::shape that determines how to compute the hash value.
+ * \param[in] window_min     The lower offset for the position of the next window from the previous one.
+ * \param[in] window_len     The upper offset for the position of the next window from the previous one.
+ * \param[in] seed           The seed used to skew the hash values. Default: 0x8F3F73B5CF1C9ADE.
+ * \returns                  A range of `size_t` where each value is a vector composed of two randstrobes.
+ *                           See below for the properties of the returned range.
+ * \ingroup search_views
+ *
+ * \attention
+ * Be aware of the requirements of the seqan3::views::kmer_hash view.
+ *
+ *
+ * ### View properties
+ *
+ * | Concepts and traits              | `urng_t` (underlying range type)   | `rrng_t` (returned range type)   |
+ * |----------------------------------|:----------------------------------:|:--------------------------------:|
+ * | std::ranges::input_range         | *required*                         | *preserved*                      |
+ * | std::ranges::forward_range       | *required*                         | *preserved*                      |
+ * | std::ranges::bidirectional_range |                                    | *lost*                           |
+ * | std::ranges::random_access_range |                                    | *lost*                           |
+ * | std::ranges::contiguous_range    |                                    | *lost*                           |
+ * |                                  |                                    |                                  |
+ * | std::ranges::viewable_range      | *required*                         | *guaranteed*                     |
+ * | std::ranges::view                |                                    | *guaranteed*                     |
+ * | std::ranges::sized_range         |                                    | *lost*                           |
+ * | std::ranges::common_range        |                                    | *lost*                           |
+ * | std::ranges::output_range        |                                    | *lost*                           |
+ * | seqan3::const_iterable_range     |                                    | *preserved*                      |
+ * |                                  |                                    |                                  |
+ * | std::ranges::range_reference_t   | seqan3::semialphabet               | std::size_t                      |
+ *
+ * See the views views submodule documentation for detailed descriptions of the view properties.
+ *
+ * \hideinitializer
+ *
+ */
+inline constexpr auto randstrobe2_hash = seqan3::detail::randstrobe2_hash_fn{};
+
+//!\}
+
+/*!\name Alphabet related views
+ * \{
+ */
+
+/*!\brief                    Computes randstrobes for a range with a given shape, min and max window offsets and seed.
+ * \tparam urng_t            The type of the range being processed. See below for requirements. [template parameter is
+ *                           omitted in pipe notation]
+ * \param[in] urange         The range being processed. [parameter is omitted in pipe notation]
+ * \param[in] shape          The seqan3::shape that determines how to compute the hash value.
+ * \param[in] window_min     The lower offset for the position of the next window from the previous one.
+ * \param[in] window_len     The upper offset for the position of the next window from the previous one.
+ * \param[in] seed           The seed used to skew the hash values. Default: 0x8F3F73B5CF1C9ADE.
+ * \returns                  A range of `size_t` where each value is a vector composed of two randstrobes.
+ *                           See below for the properties of the returned range.
+ * \ingroup search_views
+ *
+ * \attention
+ * Be aware of the requirements of the seqan3::views::kmer_hash view.
+ *
+ *
+ * ### View properties
+ *
+ * | Concepts and traits              | `urng_t` (underlying range type)   | `rrng_t` (returned range type)   |
+ * |----------------------------------|:----------------------------------:|:--------------------------------:|
+ * | std::ranges::input_range         | *required*                         | *preserved*                      |
+ * | std::ranges::forward_range       | *required*                         | *preserved*                      |
+ * | std::ranges::bidirectional_range |                                    | *lost*                           |
+ * | std::ranges::random_access_range |                                    | *lost*                           |
+ * | std::ranges::contiguous_range    |                                    | *lost*                           |
+ * |                                  |                                    |                                  |
+ * | std::ranges::viewable_range      | *required*                         | *guaranteed*                     |
+ * | std::ranges::view                |                                    | *guaranteed*                     |
+ * | std::ranges::sized_range         |                                    | *lost*                           |
+ * | std::ranges::common_range        |                                    | *lost*                           |
+ * | std::ranges::output_range        |                                    | *lost*                           |
+ * | seqan3::const_iterable_range     |                                    | *preserved*                      |
+ * |                                  |                                    |                                  |
+ * | std::ranges::range_reference_t   | seqan3::semialphabet               | std::size_t                      |
+ *
+ * See the views views submodule documentation for detailed descriptions of the view properties.
+ *
+ * \hideinitializer
+ *
+ */
+inline constexpr auto randstrobe3_hash = seqan3::detail::randstrobe3_hash_fn{};
+
+//!\}
---
+++ test/api/randstrobe_hash_test.cpp
@@ -0,0 +1,130 @@
+#include <forward_list>
+#include <list>
+#include <type_traits>
+
+#include <seqan3/alphabet/container/bitpacked_sequence.hpp>
+#include <seqan3/alphabet/detail/debug_stream_alphabet.hpp>
+#include <seqan3/alphabet/nucleotide/dna4.hpp>
+#include <seqan3/alphabet/views/complement.hpp>
+#include <seqan3/io/views/detail/take_until_view.hpp>
+#include <seqan3/search/views/kmer_hash.hpp>
+
+#include <seqan3/test/expect_range_eq.hpp>
+
+#include <gtest/gtest.h>
+
+#include "randstrobe_hash.hpp"
+
+using seqan3::operator""_dna4;
+using seqan3::operator""_shape;
+using result_t = std::vector<uint64_t>;
+
+static constexpr seqan3::shape ungapped_shape = seqan3::ungapped{4};
+static constexpr seqan3::shape gapped_shape = 0b1001_shape;
+static constexpr auto ungapped_view = randstrobe2_hash(ungapped_shape, 1, 6, seqan3::seed{0});
+static constexpr auto gapped_view = randstrobe2_hash(gapped_shape, 1, 6, seqan3::seed{0});
+
+static constexpr auto ungapped3_view = randstrobe3_hash(ungapped_shape, 1, 6, seqan3::seed{0});
+static constexpr auto gapped3_view = randstrobe3_hash(gapped_shape, 1, 6, seqan3::seed{0});
+
+template <typename T>
+class randstrobe_hash_view_properties_test: public ::testing::Test { };
+
+using underlying_range_types = ::testing::Types<std::vector<seqan3::dna4>,
+                                                std::vector<seqan3::dna4> const,
+                                                //seqan3::bitpacked_sequence<seqan3::dna4>,
+                                                //seqan3::bitpacked_sequence<seqan3::dna4> const,
+                                                std::list<seqan3::dna4>,
+                                                std::list<seqan3::dna4> const>;
+
+TYPED_TEST_SUITE(randstrobe_hash_view_properties_test, underlying_range_types, );
+
+class randstrobe_hash_test : public ::testing::Test
+{
+protected:
+    std::vector<seqan3::dna4> text1{"AAAAAAAAAAAA"_dna4};
+    result_t result1{0,0,0}; // Same result for ungapped and gapped
+
+    std::vector<seqan3::dna4> text3{"ACGGCGACGTTTAG"_dna4}; // rev complement: CTAA ACGTCGCCGT
+    //                          kmers: ACGG,     CGGC,     GGCG,     GCGA,     CGAC,     GACG,     ACGT, CGTT, GTTT, TTTA, TTAG
+    //                ungapped Hashes: 26,       105,      166,      152,      97,       134,      27,   111,  191,  252,  242
+    //        reverse ungapped Hashes: 91,       150,      101,      217,      182,      109,      27,   6,     1,   192,  176
+    //                  gapped Hashes: 2,        5,        10,       8,        5,        10,       3,    7,    11,   12,   14
+    //          reverse gapped Hashes: 7,        10,        5,       13,       10,       5,        3,    2,     1,   12,    4
+    //            ungapped randstrobes: ACGGACGT, aacgcgcc, aaaccgcc, GCGAGTTT, CGACTTAG
+    //              gapped randstrobes: A--GA--T, a--gc--c, a--cc--c, G--AG--T, c--aa--t
+    // start at A ungapped randstrobes:                               GCGAGTTT, CGACTTAG
+    result_t result3_ungapped{6683, 1637, 357, 39103, 25074};
+    result_t result3_gapped{35, 37, 21, 139, 67};
+    result_t result3_ungapped_start{39103, 25074};
+    result_t result3_gapped_start{139, 67};
+
+    std::vector<seqan3::dna4> text1_3{"AAAAAAAAAAAAAAAA"_dna4};
+    result_t result3_1{0}; // Same result for ungapped and gapped
+    std::vector<seqan3::dna4> text3_3{"ACGGCGACGTTTAGGC"_dna4};
+    // ACGGCGACGTTTAG
+    //                          kmers: ACGG,     CGGC,     GGCG,     GCGA,     CGAC,     GACG,     ACGT, CGTT, GTTT, TTTA, TTAG, TAGG, AGGC
+    //                ungapped Hashes: 26,       105,      166,      152,      97,       134,      27,   111,  191,  252,  242,  202,    41
+    //                  gapped Hashes: 2,        5,        10,       8,        5,        10,       3,    7,    11,   12,   14,   16,      1
+    //            ungapped randstrobes: ACGGACGAGGC
+    //              gapped randstrobes: A--GA--TT--G
+    result_t result3_3_ungapped{1710889};
+    result_t result3_3_gapped{574};
+};
+
+template <typename adaptor_t>
+void compare_types(adaptor_t v)
+{
+    EXPECT_TRUE(std::ranges::input_range<decltype(v)>);
+    EXPECT_TRUE(std::ranges::forward_range<decltype(v)>);
+    EXPECT_FALSE(std::ranges::bidirectional_range<decltype(v)>);
+    EXPECT_FALSE(std::ranges::random_access_range<decltype(v)>);
+    EXPECT_TRUE(std::ranges::view<decltype(v)>);
+    EXPECT_FALSE(std::ranges::sized_range<decltype(v)>);
+    EXPECT_FALSE(std::ranges::common_range<decltype(v)>);
+    EXPECT_TRUE(seqan3::const_iterable_range<decltype(v)>);
+    EXPECT_FALSE((std::ranges::output_range<decltype(v), size_t>));
+}
+
+TYPED_TEST(randstrobe_hash_view_properties_test, different_input_ranges)
+{
+    TypeParam text{'A'_dna4, 'C'_dna4, 'G'_dna4, 'T'_dna4, 'C'_dna4, 'G'_dna4, 'A'_dna4, 'C'_dna4, 'G'_dna4, 'T'_dna4,
+                'T'_dna4, 'T'_dna4, 'A'_dna4, 'G'_dna4}; // ACGTCGACGTTTAG
+    result_t ungapped{6939,1633,353,49334,25074};
+    result_t gapped{51, 37, 21, 197, 67};
+    EXPECT_RANGE_EQ(ungapped, text | ungapped_view);
+    EXPECT_RANGE_EQ(gapped, text | gapped_view);
+}
+
+TEST_F(randstrobe_hash_test, ungapped)
+{
+    EXPECT_RANGE_EQ(result1, text1 | ungapped_view);
+    EXPECT_RANGE_EQ(result3_ungapped, text3 | ungapped_view);
+    EXPECT_NO_THROW(text1 | randstrobe2_hash(ungapped_shape,3,6));
+    EXPECT_THROW((text3 | randstrobe2_hash(ungapped_shape,3,2)), std::invalid_argument);
+
+    EXPECT_RANGE_EQ(result3_1, text1_3 | ungapped3_view);
+    EXPECT_RANGE_EQ(result3_3_ungapped, text3_3 | ungapped3_view);
+    EXPECT_NO_THROW(text1 | randstrobe3_hash(ungapped_shape,3,6));
+    EXPECT_THROW((text3 | randstrobe3_hash(ungapped_shape,3,2)), std::invalid_argument);
+}
+
+TEST_F(randstrobe_hash_test, gapped)
+{
+    EXPECT_RANGE_EQ(result1, text1 | gapped_view);
+    EXPECT_RANGE_EQ(result3_gapped, text3 | gapped_view);
+    EXPECT_NO_THROW(text1 | randstrobe2_hash(gapped_shape, 2,5));
+    EXPECT_THROW((text3 | randstrobe2_hash(gapped_shape, 2,1)), std::invalid_argument);
+
+    EXPECT_RANGE_EQ(result3_1, text1_3 | gapped3_view);
+    EXPECT_RANGE_EQ(result3_3_gapped, text3_3 | gapped3_view);
+    EXPECT_NO_THROW(text1 | randstrobe3_hash(gapped_shape, 2,5));
+    EXPECT_THROW((text3 | randstrobe3_hash(gapped_shape, 2,1)), std::invalid_argument);
+}
+
+TEST_F(randstrobe_hash_test, combinability)
+{
+    auto start_at_a = std::views::drop(3);
+    EXPECT_RANGE_EQ(result3_ungapped_start, text3 | start_at_a | ungapped_view);
+    EXPECT_RANGE_EQ(result3_gapped_start, text3 | start_at_a | gapped_view);
+}
