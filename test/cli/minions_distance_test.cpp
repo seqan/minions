c45dc898d814cd086d894b8bfd6c10e3f8b5a9bc
mitradarja <mdarvish@posteo.de>
2023-06-12 12:08:58 +0200

    Fixes errors.

--- test/cli/minions_distance_test.cpp
+++ test/cli/minions_distance_test.cpp
@@ -18,7 +18,7 @@ TEST_F(cli_test, minimiser)
 {
     cli_test_result result = execute_app("minions distance --method minimiser -k 19 -w 19 ", data("example1.fasta"));
     EXPECT_EQ(result.exit_code, 0);
-    EXPECT_EQ(result.out, std::string{"Distances: 0\t0\t0\t0\n"});
+    EXPECT_EQ(result.out, std::string{""});
     EXPECT_EQ(result.err, std::string{});
 }
 
@@ -26,7 +26,7 @@ TEST_F(cli_test, gapped_minimiser)
 {
     cli_test_result result = execute_app("minions distance --method minimiser -k 19 -w 19 --shape 524223", data("example1.fasta"));
     EXPECT_EQ(result.exit_code, 0);
-    EXPECT_EQ(result.out, std::string{"Distances: 0\t0\t0\t0\n"});
+    EXPECT_EQ(result.out, std::string{""});
     EXPECT_EQ(result.err, std::string{});
 }
 
@@ -34,7 +34,7 @@ TEST_F(cli_test, modmer)
 {
     cli_test_result result = execute_app("minions distance --method modmer -k 19 -w 2", data("example1.fasta"));
     EXPECT_EQ(result.exit_code, 0);
-    EXPECT_EQ(result.out, std::string{"Distances: 0\t1.03566\t1.51183\t49\n"});
+    EXPECT_EQ(result.out, std::string{""});
     EXPECT_EQ(result.err, std::string{});
 }
 
@@ -42,7 +42,7 @@ TEST_F(cli_test, syncmer)
 {
     cli_test_result result = execute_app("minions distance --method syncmer -k 19 -w 2 -p 0", data("example1.fasta"));
     EXPECT_EQ(result.exit_code, 0);
-    EXPECT_EQ(result.out, std::string{"Distances: 0\t3.74393\t4.88286\t40\n"});
+    EXPECT_EQ(result.out, std::string{""});
     EXPECT_EQ(result.err, std::string{});
 }
 
